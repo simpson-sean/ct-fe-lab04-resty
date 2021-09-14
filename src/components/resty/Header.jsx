0b7b3bb5ccd9befa85d6b9cfaa21092e89f3508b
Sean <simpson.sean@outlook.com>
2021-09-14 09:18:04 -0700

    WORKDONE: initial commit, folder structure and files built

---
+++ src/components/resty/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import css from '../../css/RestyStyle.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                RESTy-
+                <span> A web-based API Server Client</span>
+            </div>
+        );
+    }
+} 
